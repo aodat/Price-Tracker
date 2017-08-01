c3f974b97d9d848c97fa224729d559277978fbb3
Abdalla Odat <macbook@Macbooks-MacBook-Pro.local>
2017-08-01 22:48:50 +0300

    Initial Commit

---
+++ Price Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Price Tracker
+//
+//  Created by Abdalla Odat on 8/1/17.
+//  Copyright © 2017 Abdalla Odat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

9b9e7ecaf5662d836a75ac9e6e8ba59ee8643763
Abdalla Odat <macbook@Macbooks-MacBook-Pro.local>
2017-08-12 19:23:19 +0300

    + Adding left button on navigation bar to open product types screen

--- Price Tracker/BaseCollectionViewController.swift
+++ Price Tracker/BaseCollectionViewController.swift
@@ -15,11 +15,26 @@ class BaseCollectionViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        leftItemBarButton()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func leftItemBarButton (){
+        let settingButton = UIBarButtonItem(title: "Setting", style: .plain, target: self, action: #selector(BaseCollectionViewController.goToProductTypes ))
+        self.navigationItem.leftBarButtonItem  = settingButton
+    }
+    
+    func goToProductTypes (){
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let homeTabViewController = mainStoryboard.instantiateViewController(withIdentifier: "ProductTypes")
+        present(homeTabViewController, animated: true, completion: nil)
+    }
+    
+    
+    
     
 }
